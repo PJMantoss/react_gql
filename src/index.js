49a12b489846ad666209180a23be022194bcfbcf
PJMantoss <joelptoss@gmail.com>
2022-04-16 19:09:20 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 
 //https://github.com/PJMantoss/react_gql
 
+const client = new ApolloClient({});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
