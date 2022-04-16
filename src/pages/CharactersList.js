2e080cdb4cda38b3b2b841568183d95f4445bfea
PJMantoss <joelptoss@gmail.com>
2022-04-16 19:31:09 +0100

    update CharactersList

--- src/pages/CharactersList.js
+++ src/pages/CharactersList.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
-const GET_CHARACTERS = gql``;
+const GET_CHARACTERS = gql`
+    query {
+        characters{
+        results {
+            id
+            name
+            image
+            gender
+        }
+        }
+    }
+`;
 
 const CharactersList = () => {
   return (
