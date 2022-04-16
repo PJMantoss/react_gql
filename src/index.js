ea148ef699654a1bfbf54c238e3b996e9dd32a7e
PJMantoss <joelptoss@gmail.com>
2022-04-16 18:57:41 +0100

    update index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//https://github.com/PJMantoss/react_gql
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
