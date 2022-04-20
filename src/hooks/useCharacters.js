d58aef8c270bfb59ec0286e15bbf842c6f3da0e3
PJMantoss <joelptoss@gmail.com>
2022-04-20 17:24:43 +0100

    update CharactersList

--- src/hooks/useCharacters.js
+++ src/hooks/useCharacters.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
+const GET_CHARACTERS = gql`
+    query {
+        characters{
+          results {
+              id
+              name
+              image
+              gender
+          }
+        }
+    }
+`;
+
 const useCharacters = () => {
   return (
     <div>useCharacters</div>
