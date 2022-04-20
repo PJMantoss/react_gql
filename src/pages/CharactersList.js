4dac9a88481806f3d71c51477bbbc4e885329f48
PJMantoss <joelptoss@gmail.com>
2022-04-20 16:49:43 +0100

    update CharactersList

--- src/pages/CharactersList.js
+++ src/pages/CharactersList.js
@@ -14,7 +14,7 @@ const GET_CHARACTERS = gql`
     }
 `;
 
-const CharactersList = () => {
+function CharactersList(){
   //const obj = useQuery(GET_CHARACTERS); 
   //Or
   const { error, loading, data} = useQuery(GET_CHARACTERS); //Destructured
