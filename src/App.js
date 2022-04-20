ea1fd1342ea473b869f6a7a742fee9846c78100a
PJMantoss <joelptoss@gmail.com>
2022-04-20 18:16:51 +0100

    update Character and App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Character from './pages/Character';
 import CharactersList from './pages/CharactersList';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route exact path='/' element={<CharactersList />} />
+        <Route path='/:id' element={<Character />} />
       </Routes>
     </div>
   );
--- src/pages/Character.js
+++ src/pages/Character.js
@@ -4,6 +4,8 @@ import { useCharacter } from '../hooks/useCharacter';
 const Character = () => {
   const { loading, error, data } = useCharacter(4);
 
+  console.log({ error, loading, data});
+
   return (
     <div>Character</div>
   )
