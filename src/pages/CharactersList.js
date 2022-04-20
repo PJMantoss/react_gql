e1c0b2cbbcc682ae2a89265f804c14522af59f7d
PJMantoss <joelptoss@gmail.com>
2022-04-20 17:25:15 +0100

    update CharactersList

--- src/pages/CharactersList.js
+++ src/pages/CharactersList.js
@@ -2,10 +2,6 @@ import React from 'react';
 import './CharactersList.css';
 
 const CharactersList = () => {
-  //const obj = useQuery(GET_CHARACTERS); 
-  //Or
-  const { error, loading, data} = useQuery(GET_CHARACTERS); //Destructured
-  console.log({ error, loading, data});
 
   if(loading) return <div>spinner...</div>
 
