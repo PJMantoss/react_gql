d1b0b57d8dbca18ac893329129a20f1b659a6a56
PJMantoss <joelptoss@gmail.com>
2022-04-20 17:46:03 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Routes } from 'react-router-dom';
 import './App.css';
 import CharactersList from './pages/CharactersList';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
       <CharactersList />
+      </Routes>
     </div>
   );
 }
