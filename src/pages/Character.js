6412841dc0f2a01a6b64c89678cbc6dd6310481c
PJMantoss <joelptoss@gmail.com>
2022-04-21 16:20:58 +0100

    update Character

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -7,6 +7,10 @@ const Character = () => {
 
   console.log({ error, loading, data});
 
+  if(loading) return <div>spinner...</div>
+
+  if(error) return <div>something went wrong...</div>
+
   return (
     <div className='character'>Character</div>
   )
