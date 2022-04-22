80a0a1ac04d6631d0df13fa59325ec1b920a8697
PJMantoss <joelptoss@gmail.com>
2022-04-22 17:36:17 +0100

    update Search component

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import { gql } from '@apollo/client';
 
-const GET_CHARACTER = gql``;
+const GET_CHARACTER = gql`
+query GetCharacterLocations() {
+  characters (filter: {
+    name: "Morty Smith"
+  }) {
+    results {
+      location {
+        name
+      }
+    }
+  }
+}
+`;
 
 const Search = () => {
     const [name, setName] = useState("");
